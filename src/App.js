9004845fb9139fafb8b0df4ccb2f9bc85baa5afd
Nandana <nandanaw@gmail.com>
2021-01-21 16:51:51 -0800

    Adds menu to navigate between levels

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LevelNav from './LevelNav.js'
+
+import { Route, Link, BrowserRouter, Switch} from 'react-router-dom';
+
 
 function App() {
+  let levelNum = 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+
+        <div className="Header">
+          <span className="Title"> Selector Safari </span>
+          <a className="TutorialButton"> Tutorial </a>
+          <Route path="/level/:levelNum" component={LevelNav}/>
+        </div>
+
+        <div className="Levels">
+          <p>
+            <Link to="/level/1"> Level 1 </Link>
+          </p>
+          <p>
+            <Link to="/level/2"> Level 2 </Link>
+          </p>
+          <p>
+            <Link to="/level/3"> Level 3 </Link>
+          </p>
+          <p>
+            <Link to="/level/4"> Level 4 </Link>
+          </p>
+          <p>
+            <Link to="/level/5"> Level 5 </Link>
+          </p>
+        </div>
+
+      </div>
+    </BrowserRouter>
+
+);
 }
 
 export default App;
