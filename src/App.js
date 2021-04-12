a0464d3a9a4e2dd0e112b13691cb6ec7bd443cf1
Nandana Suresh <nandana@Nandanas-MacBook-Pro.local>
2021-04-12 11:34:15 -0700

    Adds opening/closing functionality to the levels menu

--- src/App.js
+++ src/App.js
@@ -1,45 +1,62 @@
 import React from 'react';
 import './App.css';
 import LevelNav from './LevelNav.js'
+import LevelMenu from './LevelMenu.js'
 
-import { Route, Link, BrowserRouter, Switch} from 'react-router-dom';
+
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 
-function App() {
-  let levelNum = 1;
+class App extends React.Component{
+  state = { open: false }
+  toggleOpen = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+  render() {
+    return (
+        <BrowserRouter>
+          <div className="App">
+            <div className="Header">
+              <span className="Title"> Selector Safari </span>
+              <div className="NavButtons">
+                <a href="#/" className="TutorialButton"> Tutorial </a>
+                <Switch>
+                  <Route path="/level/1"
+                         render={(props) => (
+                          <LevelNav toggle={this.toggleOpen} show={this.state.open} levelNum="1"></LevelNav>
+                         )}
+                  />
+                  <Route path="/level/2"
+                         render={(props) => (
+                             <LevelNav toggle={this.toggleOpen} show={this.state.open} levelNum="2"></LevelNav>
+                         )}
+                  />
+                  <Route path="/level/3"
+                         render={(props) => (
+                             <LevelNav toggle={this.toggleOpen} show={this.state.open} levelNum="3"></LevelNav>
+                         )}
+                  />
+                  <Route path="/level/4"
+                         render={(props) => (
+                             <LevelNav toggle={this.toggleOpen} show={this.state.open} levelNum="4"></LevelNav>
+                         )}
+                  />
+                  <Route path="/"
+                         render={(props) => (
+                             <LevelNav toggle={this.toggleOpen} show={this.state.open} levelNum="1"></LevelNav>
+                             )}
+                  />
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-
-        <div className="Header">
-          <span className="Title"> Selector Safari </span>
-          <a className="TutorialButton"> Tutorial </a>
-          <Route path="/level/:levelNum" component={LevelNav}/>
-        </div>
-
-        <div className="Levels">
-          <p>
-            <Link to="/level/1"> Level 1 </Link>
-          </p>
-          <p>
-            <Link to="/level/2"> Level 2 </Link>
-          </p>
-          <p>
-            <Link to="/level/3"> Level 3 </Link>
-          </p>
-          <p>
-            <Link to="/level/4"> Level 4 </Link>
-          </p>
-          <p>
-            <Link to="/level/5"> Level 5 </Link>
-          </p>
-        </div>
-
-      </div>
-    </BrowserRouter>
-
-);
+                </Switch>
+              </div>
+            </div>
+            <LevelMenu show={this.state.open}/>
+          </div>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/LevelNav.js
+++ src/LevelNav.js
@@ -5,38 +5,47 @@ import { Link } from "react-router-dom"
 import {BiChevronLeft, BiChevronRight} from "react-icons/bi"
 
 class LevelNav extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
   render(){
-    let levelNum = this.props.match.params.levelNum;
-    let maxLevel = 5;
+    let navClass = "LevelNav"
+
+    if(this.props.show) {
+       navClass = 'LevelNav open'
+    }
+
+    let levelNum = this.props.levelNum;
+    if (levelNum === undefined){
+      levelNum = 1;
+    }
+
+    let maxLevel = 4;
     let leftButton;
     let rightButton;
 
     // make sure level number doesn't go out of bounds
-    if(levelNum == 1){
-      leftButton = <a> <Link to={`/level/${levelNum}`}> <BiChevronLeft/> </Link> </a>;
-      rightButton = <a> <Link to={`/level/${1*levelNum+1}`}> <BiChevronRight/> </Link> </a>;
+    if(levelNum === 1){
+      leftButton = <a href="#/"> <Link to={`/level/${levelNum}`}> <BiChevronLeft/> </Link> </a>;
+      rightButton = <a href="#/"> <Link to={`/level/${1*levelNum+1}`}> <BiChevronRight/> </Link> </a>;
     }
-    else if(levelNum == maxLevel){
-      leftButton = <a> <Link to={`/level/${levelNum-1}`}> <BiChevronLeft/> </Link> </a>;
-      rightButton = <a> <Link to={`/level/${1*levelNum}`}> <BiChevronRight/> </Link> </a>;
+    else if(levelNum === maxLevel){
+      leftButton = <a href="#/"> <Link to={`/level/${levelNum-1}`}> <BiChevronLeft/> </Link> </a>;
+      rightButton = <a href="#/"> <Link to={`/level/${1*levelNum}`}> <BiChevronRight/> </Link> </a>;
     }
     else {
-      leftButton = <a> <Link to={`/level/${levelNum - 1}`}> <BiChevronLeft/> </Link> </a>;
-      rightButton = <a> <Link to={`/level/${1*levelNum + 1}`}> <BiChevronRight/> </Link> </a>;
+      leftButton = <a href="#/"> <Link to={`/level/${levelNum - 1}`}> <BiChevronLeft/> </Link> </a>;
+      rightButton = <a href="#/"> <Link to={`/level/${1*levelNum + 1}`}> <BiChevronRight/> </Link> </a>;
     }
 
     return (
-      <div className="LevelNav">
-        {leftButton}
-        <p>
-          {`Level ${levelNum}`}
-        </p>
-        {rightButton}
-      </div>
+        <div className="everything">
+          <div className={navClass}>
+            {leftButton}
+            <p onClick={this.props.toggle}>
+              {`Level ${levelNum}`}
+            </p>
+            {rightButton}
+          </div>
+
+        </div>
     )
   }
 }
