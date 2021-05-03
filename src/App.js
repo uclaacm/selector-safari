e04704a784ca2d6455b99efb4844baa8b6223760
Gautier404 <and.jgautier@gmail.com>
2021-05-03 16:04:39 -0700

    levels 1-4 solution verification

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Editor from './components/Editor/Editor.js';
 // import Build from './components/Build.js'
 // import {levels} from './components/levels.js';
 
-const solution = {selectedAnimals: ['animal2', 'animal3'], style: 'color', defaultStyleValue: 'rgb(0, 0, 0)'};
-const sticker_names = ['animal'];
-
 const applyStyles = (() => {
   const style = document.createElement('style');
   document.head.append(style);
@@ -25,30 +22,17 @@ function App() {
   }
 
   const check = () => {
-    let res = true;
+    var res = true;
     let stickerStyles = {};
+    let sticker_names = ["zebra", "leopard", "acacia", "boabab", "elephantgrass", "animal"]
     // start with an array of child elements/nodes to process. we start with the children of the Board div
     let stickerNodes = Array.from(boardEl.current.children);
+    let zebraColors = []; //for plant colors test to work an element will need to be added to this array
+    let leopardColors = [];
+    let plantColors = [];
+
     while (stickerNodes.length > 0) {
       let curNode = stickerNodes.shift(); // start with the element at the front of the array
-
-      let stickerStyle = window.getComputedStyle(curNode).getPropertyValue(solution.style);
-      if (solution.style === 'color') {
-        stickerStyles[curNode.id] = stickerStyle;
-      }
-
-      if (solution.selectedAnimals.includes(curNode.id)) {
-        if (stickerStyle === solution.defaultStyleValue) {
-          // element that should be selected isn't
-          res = false;
-        }
-      }
-      else {
-        if (stickerStyle !== solution.defaultStyleValue) {
-          // element that shouldn't be selected is
-          res = false;
-        }
-      }
 
       // add child elements of the current element to the array of nodes to process
       // we wanna keep going until we process all the children
@@ -59,7 +43,49 @@ function App() {
           stickerNodes.push(child);
         }
       }
+
+      let stickerColor = window.getComputedStyle(curNode).getPropertyValue('color');  //Set the color css property to stickercolor
+      stickerStyles[curNode.id] = stickerColor;
+      //Add sticker colors to seperate arrays for analysis later
+      if (curNode.tagName === "ZEBRA"/*This will be replaced by ZEBRA*/) {
+        zebraColors.push(stickerColor);
+      }
+      else if (curNode.tagName === "LEOPARD" /*This will be replaced by LEOPARD*/) {
+        leopardColors.push(stickerColor);
+      }
+      else if (curNode.className === "plant"){
+        plantColors.push(stickerColor);
+      }
     }
+    
+    //Check that all leopard colors are the same as the background
+    //test css: animal{color:rgb(234, 216, 122)}
+    for (let s = 0; s<leopardColors.length; s++) {
+      if (leopardColors[s] !== "rgb(234, 216, 122)") {
+        res = false;
+      }
+    }
+
+    //Check that all zebra colors are different 
+    //test css: #animal1{color:rgb(200, 0, 0);} #animal2{color:rgb(0, 200, 0);}
+    for (let s = 0; s<zebraColors.length; s++ ) {
+      for (let i = s + 1; i<zebraColors.length; i++){
+        if (zebraColors[s] === zebraColors[i]) {
+          res = false;
+        }
+      }
+   }
+
+   //Check that if there are zebras that all the plants are rgb(48, 98, 48)  
+   //To test just add an element to the zebraColors array in the beggining of the function
+   //test css: .plant{color:rgb(48, 98, 48)
+   if (zebraColors.length > 0) {
+     for (let s = 0; s<plantColors.length; s++) {
+       if (plantColors[s] !== "rgb(48, 98, 48)") {
+         res = false
+       }
+     }
+   }
 
     setAnimalColors(stickerStyles);
     return res;
@@ -82,5 +108,4 @@ function App() {
     </div>
   );
 }
-//I'm making a change
 export default App;
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -10,7 +10,7 @@ const animalz = ['animal1', 'animal2', 'animal3'];
 
 function Animal(props) {
     return (
-      <animal id={props.id}>
+      <animal id={props.id} className = "plant">
         <svg width="100" height="100">
           <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="4" fill={props.color ? props.color : ""} />
         </svg>
