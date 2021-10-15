85606e51eb364be419a47ee1020c3660cf94198d
Lisha Mohan <lisha4mohan@yahoo.com>
2021-10-14 19:13:58 -0700

    remove commented out code

--- src/components/Main/check.js
+++ src/components/Main/check.js
@@ -3,9 +3,6 @@ import { sticker_names, checks } from '../../constants/constants';
 const check = (boardEl, setStickerStyles, backgroundColor) => {
   let stickerStyles = {};
   let colorsToCheck = checks.map(() => []);
-  // let zebraColors = []; //for plant colors test to work an element will need to be added to this array
-  // let leopardColors = [];
-  // let plantColors = [];
 
   let stickerNodes = Array.from(boardEl.children);
   while (stickerNodes.length > 0) {
@@ -16,15 +13,6 @@ const check = (boardEl, setStickerStyles, backgroundColor) => {
         colorsToCheck[i].push(stickerColor);
       }
     });
-    // if (curNode.tagName === "ZEBRA") {
-    //   zebraColors.push(stickerColor);
-    // }
-    // else if (curNode.tagName === "LEOPARD") {
-    //   leopardColors.push(stickerColor);
-    // }
-    // else if (curNode.className === "plant"){
-    //   plantColors.push(stickerColor);
-    // }
     stickerStyles[curNode.id] = stickerColor;
     setStickerStyles(stickerStyles);
 
@@ -36,7 +24,6 @@ const check = (boardEl, setStickerStyles, backgroundColor) => {
     });
   }
   return colorsToCheck.every((colors, i) => checks[i].check(colors, backgroundColor));
-  // checkZebras(zebraColors) && checkLeopards(leopardColors, backgroundColor) && checkPlants(plantColors);
 }
 
 export default check;
