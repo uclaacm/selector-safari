499f7cf8d1c4feddb9a45b56173e9b502342c214
evanhe1 <72584623+evanhe1@users.noreply.github.com>
2021-10-17 18:41:07 -0700

    textbox content persists for each level (#22)
    * test box content persists for each level
    
    used key prop to uniquely identify text box for each level
    
    text box content for synced with localStorage
    
    * removed debugging statements

--- src/components/LevelNav/LevelNav.js
+++ src/components/LevelNav/LevelNav.js
@@ -1,24 +1,30 @@
-import React from "react"
-import './LevelNav.css';
-import { minLevel, maxLevel } from '../../constants/constants'
-import { Link } from "react-router-dom"
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa"
+import React from "react";
+import "./LevelNav.css";
+import { minLevel, maxLevel } from "../../constants/constants";
+import { Link } from "react-router-dom";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 function LevelNav(props) {
-    const levelNum = parseInt(props.levelNum ?? minLevel);
+  const levelNum = parseInt(props.levelNum ?? minLevel);
 
-    const prevLevelNum = Math.max(levelNum - 1, minLevel);
-    const nextLevelNum = Math.min(levelNum + 1, maxLevel);
+  const prevLevelNum = Math.max(levelNum - 1, minLevel);
+  const nextLevelNum = Math.min(levelNum + 1, maxLevel);
 
-    return (
-        <div className={'LevelNav' + (props.show ? ' open' : '')}>
-            <Link to={`/level/${prevLevelNum}`}> <FaAngleLeft /> </Link>
-            <p className="Header-link" onClick={props.toggle}>
-                {`Level ${levelNum}`}
-            </p>
-            <Link to={`/level/${nextLevelNum}`}> <FaAngleRight /> </Link>
-        </div>
-    )
+  return (
+    <div className={"LevelNav" + (props.show ? " open" : "")}>
+      <Link to={`/level/${prevLevelNum}`}>
+        {" "}
+        <FaAngleLeft />{" "}
+      </Link>
+      <p className="Header-link" onClick={props.toggle}>
+        {`Level ${levelNum}`}
+      </p>
+      <Link to={`/level/${nextLevelNum}`}>
+        {" "}
+        <FaAngleRight />{" "}
+      </Link>
+    </div>
+  );
 }
 
 export default LevelNav;
