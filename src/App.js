a05a59d751a07efe0ca331eb1ba28cc6b77df8ac
Megan Go <Meganggo11@gmail.com>
2022-01-17 20:24:55 -0800

    level path restriction
    - redirects user to level 1 if level doesn't exist

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main/Main';
+import LevelNav from "./components/LevelNav/LevelNav";
 
 import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import { maxLevel } from './constants/constants';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
+function App(props) {
+    return (
+      <BrowserRouter>
+        <div className="App">
           <Route path="/level/:levelNum"
-            render={(props) => (
-              <Main {...props} />
-            )}
-          />
-          <Route path="/"
-            render={() => (
-              <Redirect to="/level/1" />
-            )}
-          />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+            render={(props) => {
+              const currLevel = props.match.params.levelNum;
+              if(currLevel > maxLevel || currLevel < 1){
+                return <Redirect to="/level/1" />
+              }
+              else{
+                return <Main {...props}/>
+              }
+            }}
+          />      
+        </div>
+      </BrowserRouter>
+    );
 }
 
 export default App;
