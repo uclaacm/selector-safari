a3feaf7bfee7b5eb308785c7183db873e7cac0a4
Nhamakami7 <nicholas_hamakami7@outlook.com>
2022-01-24 01:09:05 -0800

    implemented main tab functionality. still need to work on making the css look like the figma

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -3,9 +3,71 @@ import "./Tabs.css";
 import Textbox from "../Textbox/Textbox";
 import { levels } from "../../components/levels";
 
-// TODO: change tagList to props. tabList
 function Tabs(props) {
     const [currentTab, setCurrentTab] = useState("tab1");
+
+    console.log(props.level);
+    // definition for HTML content: 
+    let htmlContent;
+
+    if (props.level == 1) {
+        htmlContent = 
+        <div className="HTML-text">
+            <p>&#60;zebra id="zaira"/&#62;</p>
+            <p>&#60;zebra id="zed"/&#62;</p>
+            <p>&#60;zebra id="zenifer"/&#62;</p>
+        </div>
+
+    } else if (props.level == 2) {
+        htmlContent =
+        <div className="HTML-text">
+            <p>&#60;leopard id="Leonard"/&#62;</p>
+            <p>&#60;leopard id="Lily"/&#62;</p>
+        </div>
+
+    } else if (props.level == 3) {
+        htmlContent =
+        <div className="HTML-text">
+            <p>&#60;boabab id="Bob"/&#62;</p>
+            <p>&#60;boabab id="Bill"/&#62;</p>
+            <p>&#60;acacia id="Ace"/&#62;</p>
+            <p>&#60;acacia id="Aiden"/&#62;</p>
+            <p>&#60;acacia id="Alicia"/&#62;</p>
+            <p>&#60;elephentgrass id="Edward"/&#62;</p>
+            <p>&#60;elephentgrass id="Elizabeth"/&#62;</p>
+            <p>&#60;elephentgrass id="Ella"/&#62;</p>
+            <p>&#60;elephentgrass id="Eduardo"/&#62;</p>
+            <p>&#60;zebra id="zaira"/&#62;</p>
+        </div>
+
+    } else if (props.level == 4) {
+        htmlContent =
+        <div className="HTML-text">
+            <p>&#60;elephentgrass id="Edward"/&#62;</p>
+            <p>&#60;elephentgrass id="Elizabeth"/&#62;</p>
+            <p>&#60;elephentgrass id="Ella"/&#62;</p>
+            <p>&#60;elephentgrass id="Eduardo"/&#62;</p>
+            <p>&#60;zebra id="zaira"/&#62;</p>
+            <p>&#60;zebra id="zed"/&#62;</p> 
+            <p>&#60;leopard id="lilly"/&#62;</p>           
+        </div> 
+
+    } else if (props.level == 5) {
+        htmlContent =
+        <div className="HTML-text">
+            <p>&#60;leopard id="Lisa"/&#62;</p>
+                <div>
+                    <p>&#60;leopard id="Leon"/&#62;</p>
+                    <p>&#60;leopard id="Lilly"/&#62;</p>
+                    <p>&#60;zebra class="animal"/&#62;</p>
+                </div>
+            <p>&#60;zebra id="zelly"/&#62;</p>
+            <p>&#60;zebra id="zoob"/&#62;</p>        
+            <p>&#60;acacia id="Ace"/&#62;</p>
+            <p>&#60;acacia id="Aiden"/&#62;</p>     
+        </div>
+
+    }
 
     const tabList = [
         {
@@ -23,29 +85,31 @@ function Tabs(props) {
             name: 'tab2',
             label: 'HTML',
             content: (
-                <p>Testing!!!</p>
+                htmlContent
             )
         }
     ]
 
     return (
-        <div className={(currentTab === 'tab1') ? 'CSS-active-background' : 'HTML-active-background'}>
-            <div className="tabs"> 
+        <div className="tab-container">
+            <div className="button-container"> 
                 {tabList.map((tab) => (
                     <button
                         onClick={() => setCurrentTab(tab.name)}
-                        className={(tab.name === {currentTab}) ? "active-button" : "unactive-button"}
+                        className={(tab.name === currentTab) ? "active-button" : "unactive-button"}
                         >
                             {tab.label}
                     </button>
                 ))}
             </div>
+            <div className="content">
                 {tabList.map((tab) => 
                 {if (tab.name === currentTab) 
                     return tab.content;
                 else 
                     return null;
-                })}
+                })}    
+            </div>
         </div>
     )
 }
