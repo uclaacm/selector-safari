77933450b13202001d7196102edeb87df8b68121
Kyle Pu <kylez.pu@gmail.com>
2022-01-26 23:14:29 -0800

    Autoformatted Hex.js

--- src/components/Hex/Hex.js
+++ src/components/Hex/Hex.js
@@ -1,40 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
-function Hex(props){
+function Hex(props) {
     const [isCopied, setIsCopied] = useState(false);
     const [textStyle, setTextStyle] = useState();
 
     useEffect(() => {
-        if(isCopied){
+        if (isCopied) {
             setTextStyle({
                 opacity: 0.2,
                 transition: "all 2s"
             });
-            setTimeout(() => {setIsCopied(false);}, 1000);
-        }else{
+            setTimeout(() => { setIsCopied(false); }, 1000);
+        } else {
             setTextStyle({
                 opacity: 1
             });
         }
     }, [props.hexcode, isCopied]);
 
-    function hoverFocus(e){
+    function hoverFocus(e) {
         e.target.style.background = "dodgerblue";
-        setTextStyle({cursor:"pointer"});
+        setTextStyle({ cursor: "pointer" });
     }
 
-    function unfocus(e){
+    function unfocus(e) {
         e.target.style.background = "none";
     }
 
-    function handleClick(e){
+    function handleClick(e) {
         navigator.clipboard.writeText(props.hexcode);
         setIsCopied(true);
     }
 
     return (
         <span>
-            <text onMouseOver={hoverFocus} onMouseOut={unfocus} onClick={handleClick} style={{color:props.hexcode, ...textStyle}}>({isCopied ? "Copied!" : props.hexcode})</text>
+            <text onMouseOver={hoverFocus} onMouseOut={unfocus} onClick={handleClick} style={{ color: props.hexcode, ...textStyle }}>({isCopied ? "Copied!" : props.hexcode})</text>
         </span>
     );
 }
