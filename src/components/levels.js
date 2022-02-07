226ecec28d94d3079a7be9c658f61fb35fab8ec1
celebi law <celebilaw@gmail.com>
2022-02-07 00:19:59 -0800

    fixed build issues

--- src/components/levels.js
+++ src/components/levels.js
@@ -14,19 +14,6 @@ export let levels = [{
         new stickers.Zebra('zenifer', 60, 60, "#fffcf5")
     ],
     backgroundColor: "default",
-<<<<<<< HEAD
-=======
-    tutorialText: `The ID selector allows you to select an element based on its id. Each element should have a unique id, like a name. When you use the id selector it’s like you’re asking someone to do something. For example, “George, I dare you to eat this chili pepper!”
-
-In HTML, elements are given an id by using this syntax:
-<div id = “George”></div>
-
-In CSS, you can select an id by using the # symbol:
-#George {
-        color: rgb(255, 0, 0);
-}
-This will turn the element George a nice bright shade of red because they’ve just eaten a chili pepper!`,
->>>>>>> f6bf61dc13cd535fd49d22cad71d7dea1c43bfba
 },
 {
     level: 2,
@@ -38,21 +25,6 @@ This will turn the element George a nice bright shade of red because they’ve j
         new stickers.Leopard('Lily', 50, 10, "pink", null),
     ],
     backgroundColor: "default",
-<<<<<<< HEAD
-=======
-    tutorialText: `The Type selector allows you to select an element by its type. In selector safari the type of sticker will be its species. Generally, in HTML, an elements type is the word that comes after the first “<”.
-
-For example, the type of:
-<div>George</div> is a div
-<p> desperately needs milk </p> is a p
-<Zebra/> is a Zebra
-
-In CSS, you can select a type by writing the type name:
-Zebra {
-        color: rgb(100, 0, 100);
-}
-This CSS will turn all Zebras a deep shade of purple!`,
->>>>>>> f6bf61dc13cd535fd49d22cad71d7dea1c43bfba
 },
 {
     level: 3,
