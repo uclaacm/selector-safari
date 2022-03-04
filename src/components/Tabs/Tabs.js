58f4f3b3edb756781ddb70fb14392009ab0621b1
Kyle Pu <kylez.pu@gmail.com>
2022-03-04 11:20:03 -0800

    HTML descendants in Level 5 now formatted properly.
    Changed the structure of the htmlContent Array. The last field for each element in that array now tracks the number of children each element has. This allows us to differentiate between true parents (parents that have children) and what I call "non-true" parents (HTML elements that don't have any children).
    
    There are special edge cases handled with special numbers (-1 and -2) to indicate a true child (child of an element that doesn't have children itself) and closing tags (tags that close a previously opened parent element). See code for more details on this.

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -7,15 +7,33 @@ function Tabs(props) {
     const [currentTab, setCurrentTab] = useState("tab1");
 
     /* htmlContentArray formatting: element 1 - component name, element 2 - component id, 
-    element 3 - is the component a child? */
+    element 3 - number of children */
     let htmlContentArray = [];
 
     levels[props.level - 1].gamepieces.forEach((gamepiece) => {
-        htmlContentArray.push([gamepiece.constructor.name, gamepiece.name, gamepiece.class_name, false]);
+
+        /*
+        // Declare elements that don't have children as a child themselves (for easier formatting)
+        let isChild;
+        if(gamepiece.children.length){
+            isChild = false;  // Only elements with actual children will be considered parents (instead of elements that don't have children being considered parents too) for the purposes of generating the HTML code
+        }else{
+            isChild = true;
+        }
+        */
+
+        htmlContentArray.push([gamepiece.constructor.name, gamepiece.name, gamepiece.class_name, gamepiece.children.length]);
+
+        // This part adds all children element of a parent
         if (gamepiece.children) {
             gamepiece.children.forEach((child) => {
-                htmlContentArray.push([child.constructor.name, child.name, gamepiece.class_name, true]);
-            })}
+                htmlContentArray.push([child.constructor.name, child.name, gamepiece.class_name, -1]); // Use -1 as an indicator of being a true child (no children but is a child of something else)
+            })} 
+        
+        // If this gamepiece was an actual parent, add a closing tag in the HTML now
+        if(gamepiece.children.length > 0){
+            htmlContentArray.push([gamepiece.constructor.name, "", "", -2]); // Add a closing tag for parent. Use -2 as a signal for a closing tag
+        }
     });
 
     const tabList = [
@@ -38,15 +56,15 @@ function Tabs(props) {
                     {htmlContentArray.map((element) => {
                         let styling;
                         // check whether component is child or parent and applying styling accordingly
-                        if (element[3] === true)
+                        if (element[3] === -1)
                             styling = 'children';
                         else
                             styling = 'parent';
                         // check if current sticker has an id and set content accordingly
                         if (element[1] === null) 
-                            return (<p className={styling}>&#60;{element[0]} class="{element[2]}"/&#62;</p>);
+                            return (<p className={styling}>&#60;{element[0]} class="{element[2]}"{element[3] <= 0 && <txt>/</txt>}&#62;</p>);
                         else 
-                            return (<p className={styling}>&#60;{element[0]} id="{element[1]}" class="{element[2]}"/&#62;</p>);
+                            return (<p className={styling}>&#60;{element[0]} {(element[3] !== -2) && <txt>id="{element[1]}" class="{element[2]}"</txt>}{element[3] <= 0 && <txt>/</txt>}&#62;</p>);
                     })}
                 </div>
             </div>
